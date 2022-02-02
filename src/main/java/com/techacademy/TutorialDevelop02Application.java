ff3d5557052eb7f3526607b4001c955e107466c1
marimo <ma-inoue@crearaize.com>
2022-02-02 12:29:20 +0900

    Data access by Spring Data Jpa

---
+++ src/main/java/com/techacademy/TutorialDevelop02Application.java
@@ -0,0 +1,13 @@
+package com.techacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialDevelop02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialDevelop02Application.class, args);
+	}
+
+}
