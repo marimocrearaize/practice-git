725e7996ec38b264445983cd6d2078c589dab17d
marimo <ma-inoue@crearaize.com>
2022-02-09 17:56:14 +0900

    LastKadai

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -54,17 +54,39 @@ public class UserController {
                 return rtn;
         }
 
+//        課題② User登録画面からUser一覧(全件)画面に戻る
+        @RequestMapping(path="register", params="returnList")
+        public String returnList2(Model model) {
+            String rtn;
+            rtn=list(model);
+            return rtn;
+        }
         @RequestMapping(path="list", params="toChange")
         public String toChange(@RequestParam(name="idck") Integer idck, Model model) {
                 model.addAttribute("user", userService.getUser(idck));
                 return "user/change";
         }
+//        課題① User一覧(全件)画面からUser登録画面へ遷移
+        @RequestMapping(path="list", params="toRegister")
+        public String toRegister(@ModelAttribute User user,
+                                                Model model) {
+                return "user/register";
+        }
 
+//      課題③ User変更に入力チェックを追加
         @RequestMapping(path="change", params="changeRun")
-        public String changeRun(@ModelAttribute User user, Model model) {
-                userService.saveUser(user);
-                model.addAttribute("userlist", userService.getUserList());
-                return "user/list";
+        public String changeRun(@ModelAttribute @Validated User user, BindingResult res,Model model) {
+                String rtn = null;
+                if (!res.hasErrors()) { //正常値
+                    userService.saveUser(user);
+                    model.addAttribute("userlist", userService.getUserList());
+                    rtn="user/list";
+                }else { //異常値
+                    model.addAttribute("errmsg", messageSource.getMessage("errmsg.form.input", null, Locale.getDefault()));
+                    model.addAttribute("user", user);
+                    rtn = "user/change";
+                }
+                return rtn;
         }
 
         @RequestMapping(path="change", params="returnList")
--- src/main/java/com/techacademy/entity/User.java
+++ src/main/java/com/techacademy/entity/User.java
@@ -1,7 +1,5 @@
 package com.techacademy.entity;
 
-import java.io.Serializable;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -19,89 +17,175 @@ import org.hibernate.validator.constraints.Length;
 
 @Entity
 @Table(name="user")
-public class User implements Serializable{
-        private static final long serialVersionUID = 1L;
+public class User{
 
-        /** 性別の列挙 **/
-        public static enum Gender{
-                男性, 女性
-        }
 
-        /** ID */
-        @Id
-        @GeneratedValue(strategy=GenerationType.IDENTITY)
-        private Integer id;
+    /** 性別の列挙 **/
+    public static enum Gender{
+            男性, 女性
+    }
 
-        /** 名前 */
-        @Column(length=20, nullable=false)
-        @NotEmpty
-        @Length(max=20)
-        private String name;
+    /** ID */
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
 
-        /** 性別 */
-        @Column(length=2)
-        @Enumerated(EnumType.STRING)
-        private Gender gender;
+    /** 名前 */
+    @Column(length=20, nullable=false)
+    @NotEmpty
+    @Length(max=20)
+    private String name;
 
-        /** 年齢 */
-        @Min(value=0)
-        private Integer age;
+    /** 性別 */
+    @Column(length=2)
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
 
-        /** メールアドレス */
-        @Column(length=50)
-        @Email
-        @Length(max=50)
-        private String email;
+    /** 年齢 */
+    @Min(value=0)
+    private Integer age;
 
-        /** 認証エンティティと関連定義 */
-        @OneToOne(mappedBy="user")
-        private Authentication authentication;
+    /** メールアドレス */
+    @Column(length=50)
+    @Email
+    @Length(max=50)
+    private String email;
 
-        public Integer getId() {
-                return id;
-        }
+    /** 認証エンティティと関連定義 */
+    @OneToOne(mappedBy="user")
+    private Authentication authentication;
 
-        public void setId(Integer id) {
-                this.id = id;
-        }
+    public Integer getId() {
+            return id;
+    }
 
-        public String getName() {
-                return name;
-        }
+    public void setId(Integer id) {
+            this.id = id;
+    }
 
-        public void setName(String name) {
-                this.name = name;
-        }
+    public String getName() {
+            return name;
+    }
 
-        public Gender getGender() {
-                return gender;
-        }
+    public void setName(String name) {
+            this.name = name;
+    }
 
-        public void setGender(Gender gender) {
-                this.gender = gender;
-        }
+    public Gender getGender() {
+            return gender;
+    }
 
-        public Integer getAge() {
-                return age;
-        }
+    public void setGender(Gender gender) {
+            this.gender = gender;
+    }
 
-        public void setAge(Integer age) {
-                this.age = age;
-        }
+    public Integer getAge() {
+            return age;
+    }
 
-        public String getEmail() {
-                return email;
-        }
+    public void setAge(Integer age) {
+            this.age = age;
+    }
 
-        public void setEmail(String email) {
-                this.email = email;
-        }
+    public String getEmail() {
+            return email;
+    }
 
-        public Authentication getAuthentication() {
-                return authentication;
-        }
+    public void setEmail(String email) {
+            this.email = email;
+    }
 
-        public void setAuthentication(Authentication authentication) {
-                this.authentication = authentication;
-        }
+    public Authentication getAuthentication() {
+            return authentication;
+    }
+
+    public void setAuthentication(Authentication authentication) {
+            this.authentication = authentication;
+    }
 }
+//public class User implements Serializable{
+//        private static final long serialVersionUID = 1L;
+//
+//        /** 性別の列挙 **/
+//        public static enum Gender{
+//                男性, 女性
+//        }
+//
+//        /** ID */
+//        @Id
+//        @GeneratedValue(strategy=GenerationType.IDENTITY)
+//        private Integer id;
+//
+//        /** 名前 */
+//        @Column(length=20, nullable=false)
+//        @NotEmpty
+//        @Length(max=20)
+//        private String name;
+//
+//        /** 性別 */
+//        @Column(length=2)
+//        @Enumerated(EnumType.STRING)
+//        private Gender gender;
+//
+//        /** 年齢 */
+//        @Min(value=0)
+//        private Integer age;
+//
+//        /** メールアドレス */
+//        @Column(length=50)
+//        @Email
+//        @Length(max=50)
+//        private String email;
+//
+//        /** 認証エンティティと関連定義 */
+//        @OneToOne(mappedBy="user")
+//        private Authentication authentication;
+//
+//        public Integer getId() {
+//                return id;
+//        }
+//
+//        public void setId(Integer id) {
+//                this.id = id;
+//        }
+//
+//        public String getName() {
+//                return name;
+//        }
+//
+//        public void setName(String name) {
+//                this.name = name;
+//        }
+//
+//        public Gender getGender() {
+//                return gender;
+//        }
+//
+//        public void setGender(Gender gender) {
+//                this.gender = gender;
+//        }
+//
+//        public Integer getAge() {
+//                return age;
+//        }
+//
+//        public void setAge(Integer age) {
+//                this.age = age;
+//        }
+//
+//        public String getEmail() {
+//                return email;
+//        }
+//
+//        public void setEmail(String email) {
+//                this.email = email;
+//        }
+//
+//        public Authentication getAuthentication() {
+//                return authentication;
+//        }
+//
+//        public void setAuthentication(Authentication authentication) {
+//                this.authentication = authentication;
+//        }
+//}
